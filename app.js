114f5605a9b17be04e57a87866667d65abab9612
JuliaMichaela <juliaperez@yandex.ru>
2024-10-25 21:35:13 +0300

    updated js

---
+++ app.js
@@ -0,0 +1,13 @@
+const nameElement = document.getElementById('name');
+const userName = nameElement.textContent;
+
+const imageElement = document.getElementById('image');
+
+const commentDiv = document.getElementById('user-comment');
+const userComment = commentDiv.textContent;
+
+const chatContent = document.querySelector('#chat-content');
+
+
+
+
