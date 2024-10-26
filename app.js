a6cb827c16392a57e2367a8fb506bec3d3a70477
JuliaMichaela <juliaperez@yandex.ru>
2024-10-26 23:30:39 +0300

    added functions checkName, checkSpam

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
-const nameElement = document.getElementById('name');
-const userName = nameElement.textContent;
+function createNewComment () {
+    const chatContent = document.querySelector('#chatcontent');
 
-const imageElement = document.getElementById('image');
+    const author = document.querySelector('#author');
+    const authorName = author.value;
 
-const commentDiv = document.getElementById('user-comment');
-const userComment = commentDiv.textContent;
+    function checkName (authorName) {
 
-const chatContent = document.querySelector('#chat-content');
+        let nameCheck;
 
+        nameCheck = authorName.trim();
+        nameCheck = nameCheck.toLowerCase();
+        nameCheck = nameCheck.charAt(0).toUpperCase()+ nameCheck.slice(1);
+        return nameCheck;
+    }
+
+    const divName = document.createElement('div');
+    divName .innerHTML = checkName (authorName);
+    chatContent.append(divName);
+    divName.classList.add ('item-chat');
+    
+    
+    const picture = document.getElementById('picture');
+    const imageUser = document.createElement('img');
+    imageUser.src = picture.value;
+    divName.prepend(imageUser);
+    imageUser.classList.add('image');
+
+    const comment = document.querySelector('#usercomment');
+    const commentUser = comment.value;
+    
+    function checkSpam(commentUser) {
+
+        let spamCheck;
+    
+        spamCheck = commentUser.replace(/viagra/ig, "***");
+        spamCheck = spamCheck.replace(/XXX/ig, "***");
+        return spamCheck;
+    }
+
+    const userComment = document.createElement('p');
+    userComment.innerHTML = checkSpam(commentUser);
+    divName.append(userComment);
+
+    author.value = '';
+    picture.value = '';
+    comment.value = '';
+}
+
+const buttonSend = document.querySelector('#btn');
+buttonSend.addEventListener('click', createNewComment);
 
 
 
