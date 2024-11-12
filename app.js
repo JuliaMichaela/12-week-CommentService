8c9dd7f26eaed78d891743377cec138b0fc4a5bb
JuliaMichaela <juliaperez@yandex.ru>
2024-11-12 23:12:31 +0300

    edited

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ function createNewComment () {
             return nameCheck;
         }
     
+
+        
         const divName = document.createElement('div');
         divName.innerHTML = checkName (authorName);
         chatContent.append(divName);
